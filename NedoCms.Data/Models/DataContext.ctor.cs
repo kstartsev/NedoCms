c2f989fc8fac365c51e87eadd20710d50545c727
kstartsev <kstartsev@gmail.com>
2013-11-10 12:14:30 +0700

    Config cleanup

--- NedoCms.Data/Models/DataContext.ctor.cs
+++ NedoCms.Data/Models/DataContext.ctor.cs
@@ -5,6 +5,6 @@ namespace NedoCms.Data.Models
 	public partial class EditorDataContext
 	{
 		// TODO: let user specify connections string via config file
-		public EditorDataContext() : this(ConfigurationManager.ConnectionStrings["DeusEx:ConnectionString"].ConnectionString) { }
+		public EditorDataContext() : this(ConfigurationManager.ConnectionStrings["NedoCms:ConnectionString"].ConnectionString) { }
 	}
 }
